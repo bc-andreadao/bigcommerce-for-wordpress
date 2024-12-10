385ac1733246e6eaab31ddddf6e1c914c6563845
bc-andreadao <andrea.dao@bigcommerce.com>
2024-12-10 11:55:08 -0600

    add class for Flusher.php

--- src/BigCommerce/Rewrites/Flusher.php
+++ src/BigCommerce/Rewrites/Flusher.php
@@ -3,23 +3,37 @@
 
 namespace BigCommerce\Rewrites;
 
-
+/**
+ * Handles flushing and scheduling of WordPress rewrite rules for the BigCommerce plugin.
+ */
 class Flusher {
-	/**
-	 * @return void
-	 */
-	public function schedule_flush() {
-		update_option( 'bigcommerce_flushed_rewrites', 0 );
-	}
+    /**
+     * Schedules a rewrite flush by setting an option to mark rewrites as pending.
+     *
+     * This method updates the `bigcommerce_flushed_rewrites` option to `0`,
+     * signaling that a rewrite flush is required.
+     *
+     * @return void
+     */
+    public function schedule_flush() {
+        update_option( 'bigcommerce_flushed_rewrites', 0 );
+    }
 
-	/**
-	 * @return void
-	 * @action wp_loaded
-	 */
-	public function do_flush() {
-		if ( ( (int) get_option( 'bigcommerce_flushed_rewrites', 0 ) ) !== 1 ) {
-			flush_rewrite_rules();
-			update_option( 'bigcommerce_flushed_rewrites', 1 );
-		}
-	}
+    /**
+     * Executes a rewrite flush if it hasn't been performed yet.
+     *
+     * This method checks the `bigcommerce_flushed_rewrites` option. If it's not set to `1`,
+     * it flushes the rewrite rules and updates the option to `1`, preventing unnecessary future flushes.
+     *
+     * Hook: Triggered on the `wp_loaded` action.
+     *
+     * @return void
+     * @action wp_loaded
+     */
+    public function do_flush() {
+        if ( ( (int) get_option( 'bigcommerce_flushed_rewrites', 0 ) ) !== 1 ) {
+            flush_rewrite_rules();
+            update_option( 'bigcommerce_flushed_rewrites', 1 );
+        }
+    }
 }
